193da8fdbf11ce9b70508b5ed2e61922c6e96836
Sagar Mitra <mitrasagar10@gmail.com>
2025-09-03 21:01:17 +0530

    open link in new tab

--- src/Questions/QuestionNavbar.jsx
+++ src/Questions/QuestionNavbar.jsx
@@ -6,8 +6,13 @@ const QuestionNavbar = ({ title, to }) => {
     <div className="question-navbar">
       <p>{`Machine Coding Questions > ${title} `}</p>
 
-      <Link to={to} className="code-link">
-        Source Code <MdArrowOutward className="arrow"/>{" "}
+      <Link
+        to={to}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="code-link"
+      >
+        Source Code <MdArrowOutward className="arrow" />{" "}
       </Link>
     </div>
   );
