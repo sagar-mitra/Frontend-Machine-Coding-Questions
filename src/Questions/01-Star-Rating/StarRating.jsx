14cd7dd8a4e62bfd159a3dfdac2ec800915f6742
Sagar Mitra <mitrasagar10@gmail.com>
2025-09-03 20:44:40 +0530

    star-rating added

---
+++ src/Questions/01-Star-Rating/StarRating.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./star-rating.css";
+import QuestionNavbar from "../QuestionNavbar";
+
+const StarRating = ({ starCount = 5 }) => {
+  const [starVlaue, setStarValue] = useState(0);
+  const [starHoverValue, setStarHoverValue] = useState(0);
+
+  return (
+    <div className="star-rating">
+      <QuestionNavbar title="Star Rating"/>
+      <div className="star-container">
+        {new Array(starCount).fill("").map((item, index) => {
+          return (
+            <span
+              className={`star ${
+                starHoverValue === 0 && index < starVlaue && "gold"
+              } ${index < starHoverValue && "gold"} `}
+              onClick={() => setStarValue(index + 1)}
+              onMouseEnter={() => setStarHoverValue(index + 1)}
+              onMouseLeave={() => setStarHoverValue(0)}
+            >
+              &#9733;
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default StarRating;
